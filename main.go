75742cf6b4577b73ff0e29af339bb5cc5bf4cc74
vicimilenia <vici.ocbcnisp@gmail.com>
2020-07-21 18:10:19 +0700

    feat: add function to connect to database

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	_ "github.com/vicimilenia/tsaving/database"
+)
 
 func main() {
-	fmt.Println("branch develop")
+	//cara connect ke db
+	//db, _ := database.GetDatabaseConnection("host=127.0.0.1 port=5432 user=postgres password=password dbname=db sslmode=disable")
+	fmt.Println("ini branch develop")
 }

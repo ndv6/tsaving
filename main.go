cddf2bc7085e9adbfb33330e08368dda3ae9864c
Joseph Marcel <polipopoliko@gmail.com>
2020-07-21 20:55:35 +0700

    Adding basic router functionality

--- main.go
+++ main.go
@@ -5,12 +5,11 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/polipopoliko/tsaving/tsaving/api"
 	"github.com/polipopoliko/tsaving/tsaving/database"
-	_ "github.com/polipopoliko/tsaving/tsaving/database"
 )
 
 func main() {
-	//cara connect ke db
 	db, err := database.GetDatabaseConnection("host=127.0.0.1 port=5432 user=postgres password=password dbname=db sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
@@ -19,7 +18,7 @@ func main() {
 	fmt.Println(db)
 
 	fmt.Println("Server is now accepting request from port 8000")
-	err = http.ListenAndServe("127.0.0.1:8000", nil)
+	err = http.ListenAndServe("127.0.0.1:8000", api.Router(db))
 	if err != nil {
 		log.Fatal("Can not listen to port 8000", err)
 	}

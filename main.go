dabda5fd958a75fa664a78ffa8d72e3b77bfc111
vicimilenia <vici.ocbcnisp@gmail.com>
2020-07-21 18:19:48 +0700

    added requested changes

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	_ "github.com/vicimilenia/tsaving/database"
 )
 
 func main() {
 	//cara connect ke db
-	//db, _ := database.GetDatabaseConnection("host=127.0.0.1 port=5432 user=postgres password=password dbname=db sslmode=disable")
+	db, _ := database.GetDatabaseConnection("host=127.0.0.1 port=5432 user=postgres password=password dbname=db sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("ini branch develop")
 }

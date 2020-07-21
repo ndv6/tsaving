189c5997ec24e8c129d45a395b2a2697a0370ee5
Joseph Marcel <polipopoliko@gmail.com>
2020-07-21 21:07:19 +0700

    Changing database name to db_tsaving

--- main.go
+++ main.go
@@ -10,12 +10,10 @@ import (
 )
 
 func main() {
-	db, err := database.GetDatabaseConnection("host=127.0.0.1 port=5432 user=postgres password=password dbname=db sslmode=disable")
+	db, err := database.GetDatabaseConnection("host=127.0.0.1 port=5432 user=postgres password=password dbname=db_tsaving sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(db)
 
 	fmt.Println("Server is now accepting request from port 8000")
 	err = http.ListenAndServe("127.0.0.1:8000", api.Router(db))

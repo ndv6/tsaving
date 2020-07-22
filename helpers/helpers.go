e35a368d6d59546fde22f91c5195ab1f598d5b47
jocelyntjahyadi <jocelyntjahyadi26@gmail.com>
2020-07-22 10:22:23 +0700

    fungsi cek rekening

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 )
@@ -12,8 +13,28 @@ func HTTPError(w http.ResponseWriter, status int, errorMessage string) {
 }
 
 //untuk ngecek input rekening apakah benar atau tidak.
-func check_rekening() {
+func CheckRekeningVA(db *sql.DB, RekVA string) error {
+	VirtualRek := 0
+	err := db.QueryRow("SELECT va_num FROM virtual_accounts WHERE va_num = $1", RekVA).Scan(&VirtualRek)
+
+	if VirtualRek == 0 {
+		return err
+	}
+
+	return nil
 
 	// token := ch.jwt.GetToken(r)
+
+}
+
+func CheckRekening(db *sql.DB, Rek string) error {
+	NoRek := 0
+	err := db.QueryRow("SELECT account_num FROM customers WHERE account_num = $1", NoRek).Scan(&NoRek)
+
+	if NoRek == 0 {
+		return err
+	}
+
+	return nil
 
 }

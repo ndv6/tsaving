033ea72dac2ea5cbfd744a3c253905736e34b1d5
Joseph Marcel <polipopoliko@gmail.com>
2020-07-22 15:28:48 +0700

    Adding configuration and config.json to set up server port and database configuration

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,9 +3,25 @@ package helpers
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+
+	"github.com/ndv6/tsaving/models"
 )
 
 func HTTPError(w http.ResponseWriter, status int, errorMessage string) {
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(map[string]string{"error": errorMessage})
+}
+
+func LoadConfig(file string) (models.Config, error) {
+	var cfg models.Config
+	fm, err := os.Open(file)
+	if err != nil {
+		return models.Config{}, err
+	}
+	err = json.NewDecoder(fm).Decode(&cfg)
+	if err != nil {
+		return models.Config{}, err
+	}
+	return cfg, err
 }

0d6b47b20e81a31fb33c4f571aa08ae7cf43161b
David Bernadi <davidbernadi13@gmail.com>
2020-07-23 11:33:03 +0700

    fix some code due to change database balance from float to int

--- database/database_accounts.go
+++ database/database_accounts.go
@@ -7,11 +7,6 @@ import (
 )
 
 func GetBalanceAcc(accNum string, db *sql.DB) (acc models.Accounts, err error) {
-	var balanceFloat float32
-	err = db.QueryRow("SELECT account_balance FROM accounts WHERE account_num = ($1) ", accNum).Scan(&balanceFloat)
-	if err != nil {
-		return
-	}
-	acc.AccountBalance = int(balanceFloat)
+	err = db.QueryRow("SELECT account_balance FROM accounts WHERE account_num = ($1) ", accNum).Scan(&acc.AccountBalance)
 	return
 }
--- database/database_virtual_accounts.go
+++ database/database_virtual_accounts.go
@@ -7,12 +7,7 @@ import (
 )
 
 func GetBalanceVA(vaNum string, db *sql.DB) (va models.VirtualAccounts, err error) {
-	var balanceFloat float32
-	err = db.QueryRow("SELECT va_balance FROM virtual_accounts WHERE va_num = ($1) ", vaNum).Scan(&balanceFloat)
-	if err != nil {
-		return
-	}
-	va.VaBalance = int(balanceFloat)
+	err = db.QueryRow("SELECT va_balance FROM virtual_accounts WHERE va_num = ($1) ", vaNum).Scan(&va.VaBalance)
 	return
 }
 

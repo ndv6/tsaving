f47a6609ac8dddeb72f39d61889fe1913f2c4ea8
David Bernadi <davidbernadi13@gmail.com>
2020-07-23 10:51:24 +0700

    pindah fungsi check balance ke database

--- database/database_virtual_accounts.go
+++ database/database_virtual_accounts.go
@@ -15,3 +15,27 @@ func GetBalanceVA(vaNum string, db *sql.DB) (va models.VirtualAccounts, err erro
 	va.VaBalance = int(balanceFloat)
 	return
 }
+
+func CheckBalance(target string, accNumber string, amount int, db *sql.DB) (status bool) {
+	if target == "MAIN" {
+		sourceBalance, err := GetBalanceAcc(accNumber, db)
+		if err != nil {
+			return
+		}
+		if sourceBalance.AccountBalance < amount || amount <= 0 {
+			return
+		}
+		status = true
+	}
+	if target == "VA" {
+		sourceBalance, err := GetBalanceVA(accNumber, db)
+		if err != nil {
+			return
+		}
+		if sourceBalance.VaBalance < amount || amount <= 0 {
+			return
+		}
+		status = true
+	}
+	return
+}
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,38 +1,11 @@
 package helpers
 
 import (
-	"database/sql"
 	"encoding/json"
 	"net/http"
-
-	"github.com/ndv6/tsaving/database"
 )
 
 func HTTPError(w http.ResponseWriter, status int, errorMessage string) {
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(map[string]string{"error": errorMessage})
-}
-
-func CheckBalance(target string, accNumber string, amount int, db *sql.DB) (status bool) {
-	if target == "MAIN" {
-		sourceBalance, err := database.GetBalanceAcc(accNumber, db)
-		if err != nil {
-			return
-		}
-		if sourceBalance.AccountBalance < amount || amount <= 0 {
-			return
-		}
-		status = true
-	}
-	if target == "VA" {
-		sourceBalance, err := database.GetBalanceVA(accNumber, db)
-		if err != nil {
-			return
-		}
-		if sourceBalance.VaBalance < amount || amount <= 0 {
-			return
-		}
-		status = true
-	}
-	return
 }

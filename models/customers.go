5dabfda72aa8b4fe29ebd091a0e2691c33a01a7c
Caesar Pamungkas <caesargusti@gmail.com>
2020-07-23 13:51:07 +0700

    Requirement Register

--- models/customers.go
+++ models/customers.go
@@ -13,6 +13,7 @@ type Customers struct {
 	CustPhone 		string 		`json:"cust_phone"`
 	CustEmail 		string 		`json:"cust_email"`
 	CustPassword 	string 		`json:"cust_password"`
+	CustPict		string		`json:"cust_pict"`
 	IsVerified 		bool 		`json:"is_verified"`
 	Channel 		string 		`json:"channel"`
 	CreatedAt 		time.Time 	`json:"created_at"`
@@ -20,7 +21,7 @@ type Customers struct {
 }
 
 
-func RegisterCustomer(db *sql.DB, objCustomer Customers, AccNum string) error {
+func RegisterCustomer(db *sql.DB, objCustomer Customers, AccNum string, Pass string) error {
 	Create := time.Now()
 	Update := time.Now()
 	Verified := false
@@ -29,7 +30,7 @@ func RegisterCustomer(db *sql.DB, objCustomer Customers, AccNum string) error {
 			objCustomer.CustAddress,
 			objCustomer.CustPhone,
 			objCustomer.CustEmail,
-			objCustomer.CustPassword,
+			Pass,
 			Verified,
 			objCustomer.Channel,
 			Create,

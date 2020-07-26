1ee6a32f6dbda010483d12881737ea9f439f4ddc
Caesar Pamungkas <caesargusti@gmail.com>
2020-07-26 07:25:03 +0700

    Fix Response Login and Register

--- api/customers/login.go
+++ api/customers/login.go
@@ -3,17 +3,23 @@ package customers
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/jwtauth"
+	"github.com/ndv6/tsaving/constants"
 	"github.com/ndv6/tsaving/helpers"
 	"github.com/ndv6/tsaving/models"
 	"github.com/ndv6/tsaving/tokens"
 )
 
 var JWT = jwtauth.New("HS256", []byte("secret"), nil)
+
+const (
+	LoginSucceed = "Login Succeed"
+)
 
 type LoginRequest struct {
 	CustEmail    string `json:"cust_email"`
@@ -24,12 +30,12 @@ type LoginResponse struct {
 	Token string `json:"token"`
 }
 
-func LoginHandler(jwt *tokens.JWT, db *sql.DB) http.HandlerFunc {
+func LoginHandler(jwt *tokens.JWT, db *sql.DB) http.HandlerFunc { // Handle by Caesar Gusti
 	return func(w http.ResponseWriter, r *http.Request) {
 		var l LoginRequest // Ngambil dari body API
 		err := json.NewDecoder(r.Body).Decode(&l)
 		if err != nil {
-			helpers.HTTPError(w, http.StatusBadRequest, "Unable parse Request") //Format JSON Tidak Sesuai
+			helpers.HTTPError(w, http.StatusBadRequest, constants.CannotReadRequest) //Format JSON Tidak Sesuai
 			return
 		}
 
@@ -51,10 +57,13 @@ func LoginHandler(jwt *tokens.JWT, db *sql.DB) http.HandlerFunc {
 			Token: tokenLogin,
 		}
 
-		err = json.NewEncoder(w).Encode(data)
+		_, res, err := helpers.NewResponseBuilder(w, true, LoginSucceed, data)
+
 		if err != nil {
-			helpers.HTTPError(w, http.StatusBadRequest, "Unable to Encode response")
+			helpers.HTTPError(w, http.StatusInternalServerError, constants.CannotEncodeResponse)
 			return
 		}
+
+		fmt.Fprint(w, string(res))
 	}
 }

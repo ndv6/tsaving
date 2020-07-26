199085e036e83017f9a327df3c60543feaf95d26
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-07-26 10:14:58 +0700

    add name and set header response

--- api/customers/history_transaction.go
+++ api/customers/history_transaction.go
@@ -19,27 +19,32 @@ func (ch *CustomerHandler) HistoryTransactionHandler(db *sql.DB) http.HandlerFun
 		token := ch.jwt.GetToken(r)
 		err := token.Valid()
 		if err != nil {
+			w.Header().Set(constants.ContentType, constants.Json)
 			helpers.HTTPError(w, http.StatusBadRequest, err.Error())
 			return
 		}
 
 		page, err := strconv.Atoi(chi.URLParam(r, "page"))
 		if err != nil {
+			w.Header().Set(constants.ContentType, constants.Json)
 			helpers.HTTPError(w, http.StatusBadRequest, constants.CannotParseURLParams)
 			return
 		}
 
 		listHistoryTransaction, err := models.ListTransactionLog(db, token.CustId, page)
 		if err != nil {
+			w.Header().Set(constants.ContentType, constants.Json)
 			helpers.HTTPError(w, http.StatusBadRequest, "Cannot get history transaction")
 			return
 		}
 		_, res, err := helpers.NewResponseBuilder(w, true, constants.GetListSuccess, listHistoryTransaction)
 		if err != nil {
+			w.Header().Set(constants.ContentType, constants.Json)
 			helpers.HTTPError(w, http.StatusBadRequest, constants.CannotEncodeResponse)
 			return
 		}
 
+		w.Header().Set(constants.ContentType, constants.Json)
 		fmt.Fprint(w, string(res))
 	})
 }

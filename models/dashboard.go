4b27efffb60d392bdc36eeb8b2c009d1dd20ebe3
David Bernadi <davidbernadi13@gmail.com>
2020-07-29 21:51:36 +0700

    refactor models name

--- models/dashboard.go
+++ models/dashboard.go
@@ -2,7 +2,7 @@ package models
 
 type Dashboard struct {
 	CustName       string            `json:"cust_name"`
-	CustPhone      string            `json:"cust_phone"`
+	CustEmail      string            `json:"cust_email"`
 	AccountNum     string            `json:"account_num"`
 	AccountBalance int               `json:"account_balance"`
 	ListVA         []VirtualAccounts `json:"virtual_accounts"`

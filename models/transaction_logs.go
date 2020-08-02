30e259178e7e87e122c53b9cfc13e3b1de08b27e
vicimilenia <vici.ocbcnisp@gmail.com>
2020-08-02 20:30:29 +0700

    revert unintended change

--- models/transaction_logs.go
+++ models/transaction_logs.go
@@ -56,8 +56,8 @@ func ListTransactionLog(db *sql.DB, id int, page int) (list []HistoryTransaction
 		return
 	}
 
-	offset := (page - 1) * 6
-	rows, err := db.Query("SELECT account_num, dest_account, from_account, tran_amount, description, created_at FROM transaction_logs WHERE account_num = $1 ORDER BY created_at OFFSET $2 LIMIT 6", accNumber, offset)
+	offset := (page - 1) * 20
+	rows, err := db.Query("SELECT account_num, dest_account, from_account, tran_amount, description, created_at FROM transaction_logs WHERE account_num = $1 ORDER BY created_at OFFSET $2 LIMIT 20", accNumber, offset)
 	if err != nil {
 		return
 	}

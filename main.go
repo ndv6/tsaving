7d86dcf2be15535abd7ec4f4661afc7b296ce8bd
chazuka <hello@komang.id>
2020-08-05 00:53:32 +0700

    optional .env

--- main.go
+++ main.go
@@ -13,10 +13,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 
 	port := os.Getenv("PORT")
 	if port == "" {

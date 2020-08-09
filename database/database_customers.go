6f84c712247526b253094b14f8d7779c7f5eb46a
David Bernadi <davidbernadi13@gmail.com>
2020-08-09 23:49:38 +0700

    modif minor query customer

--- database/database_customers.go
+++ database/database_customers.go
@@ -21,8 +21,7 @@ func GetListCustomers(db *sql.DB, page int, date string, keyword string) (list [
 
 	offset := (page - 1) * 20
 
-
-	query := fmt.Sprintf("SELECT COALESCE(account_num,'') as account_num, cust_name, cust_address, cust_phone, cust_email, cust_password, COALESCE(is_verified, false) as is_verified, COALESCE(channel,'') as channel, COALESCE(card_num,'') as card_num, COALESCE(cvv,'') as cvv, COALESCE(expired,now()) as expired, COALESCE(created_at,now()) as created_at, COALESCE(updated_at,now()) as updated_at, COALESCE(is_deleted,'1970-01-01 08:00:00') as is_deleted FROM customers WHERE 1 = 1 %v ORDER BY created_at DESC OFFSET %v LIMIT 20", where, strconv.Itoa(offset))
+	query := fmt.Sprintf("SELECT cust_id, COALESCE(account_num,'') as account_num, cust_name, cust_address, cust_phone, cust_email, cust_password, COALESCE(is_verified, false) as is_verified, COALESCE(channel,'') as channel, COALESCE(card_num,'') as card_num, COALESCE(cvv,'') as cvv, COALESCE(expired,now()) as expired, COALESCE(created_at,now()) as created_at, COALESCE(updated_at,now()) as updated_at, COALESCE(is_deleted,'1970-01-01 08:00:00') as is_deleted FROM customers WHERE 1 = 1 %v ORDER BY created_at DESC OFFSET %v LIMIT 20", where, strconv.Itoa(offset))
 
 	rows, err := db.Query(query)
 	if err != nil {
@@ -33,7 +32,7 @@ func GetListCustomers(db *sql.DB, page int, date string, keyword string) (list [
 	defer rows.Close()
 	for rows.Next() {
 		var cus models.Customers
-		err = rows.Scan(&cus.AccountNum, &cus.CustName, &cus.CustAddress, &cus.CustPhone, &cus.CustEmail, &cus.CustPassword, &cus.IsVerified, &cus.Channel, &cus.CardNum, &cus.Cvv, &cus.Expired, &cus.CreatedAt, &cus.UpdatedAt, &cus.IsDeleted)
+		err = rows.Scan(&cus.CustId, &cus.AccountNum, &cus.CustName, &cus.CustAddress, &cus.CustPhone, &cus.CustEmail, &cus.CustPassword, &cus.IsVerified, &cus.Channel, &cus.CardNum, &cus.Cvv, &cus.Expired, &cus.CreatedAt, &cus.UpdatedAt, &cus.IsDeleted)
 		if err != nil {
 			return
 		}
